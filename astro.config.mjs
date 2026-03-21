7f1207c3acf96ab3dce9eaa252cfbb27e5166a1d
infinite-kemonoyagi <infidoggy.newgrounds@gmail.com>
2026-03-21 14:58:18 -0700

    resources is setted as the public directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   srcDir: "source",
+  publicDir: "resources",
+
+  site: "https://infinite-kemonoyagi.github.io/",
+
+  build: {
+    format: "file",
+    assets: "resources",
+  },
 });
